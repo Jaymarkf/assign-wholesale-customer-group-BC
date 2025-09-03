b373fb4d59f9cb3bdbaf2946419a57288dd2dc0c
maxx <fernandesjaymark@yahoo.com>
2025-09-03 19:00:58 +0800

    hel

--- functions/customer.js
+++ functions/customer.js
@@ -1,20 +1,5 @@
 export async function onRequestGet(context) {
-  const { BC_STORE_HASH, BC_API_TOKEN } = context.env;
-
-  const url = `https://api.bigcommerce.com/stores/${BC_STORE_HASH}/v3/customers`;
-
-  const response = await fetch(url, {
-    method: "GET",
-    headers: {
-      "X-Auth-Token": BC_API_TOKEN,
-      "Accept": "application/json",
-      "Content-Type": "application/json"
-    }
-  });
-
-  const data = await response.json();
-
-  return new Response(JSON.stringify(data, null, 2), {
+  return new Response(JSON.stringify({ message: "test ok" }), {
     headers: { "Content-Type": "application/json" }
   });
 }
