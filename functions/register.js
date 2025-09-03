6ecc935b304064ede12c80b505f03da9dbf26028
maxx <fernandesjaymark@yahoo.com>
2025-09-03 18:17:38 +0800

    .

--- functions/register.js
+++
@@ -1,6 +0,0 @@
-// functions/register.js
-export async function onRequestPost({ request, env }) {
-  return new Response(JSON.stringify({ message: "Register working!" }), {
-    headers: { "Content-Type": "application/json" },
-  });
-}
